f4bbf532affcf7ea7d771559389273b1b85c4d74
AsSidiiqqi12 <151488296+AsSidiiqqi12@users.noreply.github.com>
2023-11-20 20:45:34 +0700

    Add files via upload

--- Solo.java
+++ Solo.java
@@ -1,24 +1,14 @@
-import  java.util.Scanner;
 
 public class Solo {
     public static void main(String[] args) {
-        String[] menu = {"Tea", "Espresso", "Americano", "Water", "Hot Chocolate"};
-        Scanner sc = new Scanner(System.in);
-        //your code goes here
-        int a;
-        if (sc.hasNextInt()) {
-            a = sc.nextInt();
-            if (a >= 0 && a < menu.length){
-                String selectedDrink = menu[a];
-                System.out.println(selectedDrink);
-            }else{
-                System.out.println("Invalid");
-            }
-        }else{
-            System.out.println("Invalid input. please enter a valid integer.");
+        boolean a = true;
+        if (!a){
+            System.out.println("b");
 
+
+        }else{
+            System.out.println("c");
         }
-        sc.close();
     }
 
 }
