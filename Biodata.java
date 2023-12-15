1bb7086f907c28fd7ce516b3d97de22303d160f6
AsSidiiqqi12 <151488296+AsSidiiqqi12@users.noreply.github.com>
2023-12-15 16:51:00 +0700

    Add files via upload

--- Biodata.java
+++ Biodata.java
@@ -1,11 +1,40 @@
-public class Biodata {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+
+public class Biodata extends JFrame {
+    private JTextField textField1;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JComboBox comboBox3;
+    private JTextArea textArea1;
+    private JButton button1;
+    private JPanel mainPanel;
+    private JLabel jNama;
+    private JLabel nama;
+    private JTextField textField2;
+
+    public Biodata(){
+        setContentPane(mainPanel);
+        setSize(450, 300);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String NAMA = textField1.getText();
+                Action tanggal =comboBox1.getAction();
+                textField2.setText(NAMA);
+
+            }
+        });
+    }
+
     public static void main(String[] args) {
-        String nama ="As siddiqi Pohan";
-        String alamat ="JL Garuda Sakti perum unri km 2";
-        int age = 18;
-        System.out.println( " Nama   :  " + nama);
-        System.out.println( " Alamat :  " + alamat);
-        System.out.println( " Umur   :  " + age + " tahun ");
+        Biodata biodata = new Biodata();
+
 
     }
 }
